44900bfc6a3dc7ecf9d2c2e0e3cc192678491a13
Guanbao Liang <2031808280@qq.com>
2025-07-15 23:27:53 +0800

    update gsm8k

--- src/config/dataset_config.py
+++ src/config/dataset_config.py
@@ -8,5 +8,15 @@ supported_dataset = {
         "label_key": "label",
         "input_key": "text",
         "default_prompt": "# Task\nDetermine whether the Statement is a lie (Yes) or not (No) based on the Context and other information.\n\n# Output format\nAnswer Yes or No as labels\n\n# Prediction\nText: {text}\nLabel:"
+    },
+    "gsm8k": {
+        "dataset_name": "GSM8K",
+        "data_path": [
+            "data/gsm8k/main/train-00000-of-00001.parquet",
+            "data/gsm8k/main/test-00000-of-00001.parquet",
+        ],
+        "label_key": "answer",
+        "input_key": "question",
+        "default_prompt": "Question: {question}\nAnswer:"
     }
 }
--- src/dataset/__init__.py
+++ src/dataset/__init__.py
@@ -1,2 +1,3 @@
 from dataset.Liar import Liar
+from dataset.GSM8K import GSM8K
 from dataset.auto_dataset import AutoDataset
