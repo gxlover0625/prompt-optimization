2324ad86173b9f674ed30821cecdfd7b32761234
Guanbao Liang <2031808280@qq.com>
2025-07-15 18:06:53 +0800

    update common->llms

--- src/common/__init__.py
+++
@@ -1,9 +0,0 @@
-from common.openai_backend import OpenAIBackend
-from common.ollama_backend import OllamaBackend
-from common.vllm_backend import VLLMBackend
-
-backend = {
-    "openai": OpenAIBackend,
-    "ollama": OllamaBackend,
-    "vllm": VLLMBackend,
-}
