b4e60a5c0f72c67a1de11e10044684d670f17920
Guanbao Liang <2031808280@qq.com>
2025-07-15 18:25:34 +0800

    update pipline

--- src/pipline/direct.py
+++ src/pipline/direct.py
@@ -24,16 +24,26 @@ class ExecutionAgent(Agent):
 class DirectPipline(Pipline):
     def __init__(self, cfg:Dict):
         super().__init__(cfg)
+        self.build_pipline()
+    
+    def build_pipline(self):
+        self.execution_agent = ExecutionAgent(self.cfg)
     
     def run(self):
         pass
 
 if __name__ == "__main__":
     llm_cfg = supported_llm["qwen3-14b_vllm"]
-    agent = ExecutionAgent(llm_cfg)
-    print(agent.llm.chat("你好"))
+    # agent = ExecutionAgent(llm_cfg)
+    # print(agent.llm.chat("你好"))
 
     dataset_cfg = supported_dataset["liar"]
-    dataset = Liar(dataset_cfg)
-    print(len(dataset.split["train"]),len(dataset.split["test"]))
-    print(dataset.split["train"][0])
+    # dataset = Liar(dataset_cfg)
+    # print(len(dataset.split["train"]),len(dataset.split["test"]))
+    # print(dataset.split["train"][0])
+
+    ## 将llm_cfg和dataset_cfg合并成pipline_cfg
+    pipline_cfg = {**llm_cfg, **dataset_cfg}
+    print(pipline_cfg)
+    pipline = DirectPipline(pipline_cfg)
+    print(pipline.execution_agent.llm.chat("你好"))
