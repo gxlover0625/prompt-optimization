7d54ab0ba3b370dedcf60e373c936e7cfe6d32e0
Guanbao Liang <2031808280@qq.com>
2025-07-15 18:04:47 +0800

    update dataset

--- src/configs/dataset_config.py
+++ src/configs/dataset_config.py
@@ -3,6 +3,9 @@ supported_dataset = {
         "data_path": [
             "data/liar/train.jsonl",
             "data/liar/test.jsonl",
-        ]
+        ],
+        "label_key": "label",
+        "input_key": "text",
+        "default_prompt": "# Task\nDetermine whether the Statement is a lie (Yes) or not (No) based on the Context and other information.\n\n# Output format\nAnswer Yes or No as labels\n\n# Prediction\nText: {text}\nLabel:"
     }
 }
--- src/piplines/direct.py
+++ src/piplines/direct.py
@@ -2,7 +2,8 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from configs import supported_llm
+from configs import supported_llm, supported_dataset
+from datasets import Liar
 from typing import Dict, List
 from core.agent import Agent
 from common import backend
@@ -17,6 +18,10 @@ class ExecutionAgent(Agent):
 
 if __name__ == "__main__":
     llm_cfg = supported_llm["qwen3-14b_vllm"]
-    print(llm_cfg)
     agent = ExecutionAgent(llm_cfg)
     print(agent.llm.chat("你好"))
+
+    dataset_cfg = supported_dataset["liar"]
+    dataset = Liar(dataset_cfg)
+    print(len(dataset.split["train"]),len(dataset.split["test"]))
+    print(dataset.split["train"][0])
