7ed0a765b2f62168b522471da33da25400eceb93
Guanbao Liang <2031808280@qq.com>
2025-07-15 18:18:11 +0800

    update pipline

--- src/pipline/direct.py
+++ src/pipline/direct.py
@@ -4,16 +4,28 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from config import supported_llm, supported_dataset
 from dataset import Liar
-from typing import Dict, List
 from core.agent import Agent
+from core.pipline import Pipline
 from llm import backend
+
+from typing import Dict, List
 
 class ExecutionAgent(Agent):
     def __init__(self, cfg:Dict):
         super().__init__(cfg)
         self.llm = backend[cfg["backend"]](cfg)
     
+    def execute(self, example:Dict):
+        pass
+
     def run(self, examples:List[Dict]):
+        pass
+
+class DirectPipline(Pipline):
+    def __init__(self, cfg:Dict):
+        super().__init__(cfg)
+    
+    def run(self):
         pass
 
 if __name__ == "__main__":
