90acacf6969c263f293e7e9a7aee15d71593e3d3
Guanbao Liang <2031808280@qq.com>
2025-07-15 16:46:14 +0800

    update thinking llm

--- src/piplines/direct.py
+++ src/piplines/direct.py
@@ -3,7 +3,7 @@ import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from configs import supported_llm
-from typing import Dict
+from typing import Dict, List
 from core.agent import Agent
 from common import backend
 
@@ -12,10 +12,11 @@ class ExecutionAgent(Agent):
         super().__init__(cfg)
         self.llm = backend[cfg["backend"]](cfg)
     
-    def run(self, *args, **kwargs):
+    def run(self, examples:List[Dict]):
         pass
 
 if __name__ == "__main__":
     llm_cfg = supported_llm["qwen3-14b_vllm"]
+    print(llm_cfg)
     agent = ExecutionAgent(llm_cfg)
     print(agent.llm.chat("你好"))
