d07894e73b5d05c489601d71e5db1132acf79b48
Guanbao Liang <2031808280@qq.com>
2025-07-15 18:25:34 +0800

    update pipline

--- src/core/pipline.py
+++ src/core/pipline.py
@@ -4,6 +4,10 @@ from typing import Dict
 class Pipline(ABC):
     def __init__(self, cfg:Dict):
         self.cfg = cfg
+    
+    @abstractmethod
+    def build_pipline(self, *args, **kwargs):
+        pass
 
     @abstractmethod
     def run(self, *args, **kwargs):
