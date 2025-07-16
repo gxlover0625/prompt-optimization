b9ada979436999329c3e41eba3c862dbd2aedbc4
Guanbao Liang <2031808280@qq.com>
2025-07-16 22:55:00 +0800

    update zerocot pipline

---
+++ examples/zerocot_pipline.sh
@@ -0,0 +1,17 @@
+# change the working directory
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+PARENT_DIR=$(dirname "$SCRIPT_DIR")
+cd "$PARENT_DIR"
+
+# set the parameters
+model="qwen3-14b_vllm_non-thinking"
+dataset="liar"
+pipline="zerocot"
+output_dir="output"
+
+# run the main script
+python src/main.py \
+    --pipline $pipline \
+    --model $model \
+    --dataset $dataset \
+    --output_dir $output_dir
