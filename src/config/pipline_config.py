ecc386194bb804ed575c9cca80417b08f738b04f
Guanbao Liang <2031808280@qq.com>
2025-07-17 16:30:45 +0800

    update readme

--- src/config/pipline_config.py
+++ src/config/pipline_config.py
@@ -25,5 +25,14 @@ supported_pipline = {
         "evaluation_agent": None,
         "default_prompt": " Please first think about the principles involved in solving this task which could be helpful. And Then provide a solution step by step for this question. Ensure the response concludes with the answer in the format: <answer>answer</answer>.",
         "do_postprocess": True
+    },
+    "rephrase": {
+        "pipline": "Rephrase",
+        "output_dir": "output",
+        "pipline_class": "pipline.rephrase.RephrasePipline",
+        "execution_agent": None,
+        "evaluation_agent": None,
+        "default_prompt": "\nRephrase and expand the question, and respond. Ensure the response concludes with the answer in the format: <answer>answer</answer>.",
+        "do_postprocess": True
     }
 }
