134579b6ab7a82a15dddb4d42e722f287343cb29
Guanbao Liang <2031808280@qq.com>
2025-08-05 04:45:26 +0000

    update liar dataset in promptbreeder

--- promptbreeder/dataset.py
+++ promptbreeder/dataset.py
@@ -18,6 +18,38 @@ class Dataset(ABC):
     def load(self, *args, **kwargs):
         pass
 
+class Liar(Dataset):
+    def __init__(self, cfg:Dict):
+        super().__init__(cfg)
+        self.load()
+    
+    def load(self):
+        data_path = self.cfg["data_path"]
+        if isinstance(data_path, str):
+            self.train_data = self.load_data(data_path)
+            self.split = {"train": self.train_data, "test": None}
+        elif isinstance(data_path, List):
+            self.train_data = self.load_data(data_path[0])
+            self.test_data = self.load_data(data_path[1])
+            self.split = {"train": self.train_data, "test": self.test_data}
+    
+    def load_data(self, data_path:str)-> List[Dict]:
+        data = []
+        with open(data_path, "r") as f:
+            for line in f:
+                data.append(json.loads(line))
+        return data
+
+    def build_prompt(self, example:Dict):
+        default_prompt = self.cfg["default_prompt"]
+        input_key = self.cfg["input_key"]
+        prompt = default_prompt.format(**{input_key: example[input_key]})
+        return prompt
+    
+    # copy from https://github.com/microsoft/LMOps/blob/main/prompt_optimization/predictors.py
+    def evaluate(self, model_prediction:str, label:int):
+        extracted_prediction = 1 if model_prediction.strip().upper().startswith('YES') else 0
+        return extracted_prediction == label
 class GSM8K(Dataset):
     def __init__(self, cfg:Dict):
         super().__init__(cfg)
@@ -126,6 +158,7 @@ class BBHObjectCounting(Dataset):
         return self.is_equal(model_prediction, label)
 
 dataset_mapping = {
+    "Liar": Liar,
     "GSM8K": GSM8K,
     "BBHObjectCounting": BBHObjectCounting,
 }
